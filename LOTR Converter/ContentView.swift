839b02033a0787ae46573fce7af6114c2485a6f8
Rich <richschreiber10@gmail.com>
2023-06-13 22:55:24 -0400

    End of Lesson 2

--- LOTR Converter/ContentView.swift
+++ LOTR Converter/ContentView.swift
@@ -11,44 +11,93 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             // Background Image
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
             VStack {
-                // pony image
+                // Prancing Pony
+                Image("prancingpony")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // currency exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
                 
                 // coversion section
                 HStack {
                     // left conversion section
                     VStack {
                         // currency
+                    
                         HStack {
                             //currency image
+                            Image("silverpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
                             //currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundColor(.white)
                         }
                         
                         //text field
+                        Text("text field")
                     }
                     
                     // equal sign
-                    
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        
                     
                     // right conversion section
-                }
-                VStack {
-                    // currency
-                    HStack {
-                        //currency text
-                        //currency image
+                    VStack {
+                        // currency
+                        HStack {
+                            //currency text
+                            Text("Gold Piece")
+                                .foregroundColor(.white)
+                            
+                            
+                            //currency image
+                            Image("goldpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        
+                        //text field
+                        Text("text field")
+
                     }
                     
-                    //text field
                 }
                 
-                // info button
+                Spacer()
                 
+                // info button
+                HStack {
+                    Spacer()
+                    
+                    
+                    Button {
+                        // Display exchange info screen
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                    }
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                }
+                                
             }
         }
-        
     }
 }
 
