fad80ffac1a4d8ff858843d84e5d8d5a60c31694
Rich <richschreiber10@gmail.com>
2023-06-13 21:17:17 -0400

    Initial commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Rich Schreiber on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
